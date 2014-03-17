71c0bafc379386cd2f145c2d87dbdd341f5ab48c
ryancarlson <ryan.t.carlson@cru.org>
2014-03-17 16:23:46 -0400

    Handle filename hashing both in the contents file and the page content IDs #Adding files for kgp en translation  #- parsing all the XML correctly, now what to do with it? # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # On branch api-stubs # Changes to be committed: #   (use "git reset HEAD <file>..." to unstage) # #	modified:   pom.xml #	new file:   src/main/java/org/cru/godtools/api/packages/MockPackageService.java #	modified:   src/main/java/org/cru/godtools/api/packages/PackageResource.java #	new file:   src/main/resources/data/packages-en-kgp.xml # # Untracked files: #   (use "git add <file>..." to include in what will be committed) # #	src/main/resources/data/packages/

--- src/main/java/org/cru/godtools/api/packages/PackageResponse.java
+++ src/main/java/org/cru/godtools/api/packages/PackageResponse.java
@@ -2,6 +2,9 @@ package org.cru.godtools.api.packages;
 
 import org.jboss.resteasy.plugins.providers.multipart.MultipartRelatedOutput;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -26,13 +29,37 @@ public class PackageResponse
     {
         MultipartRelatedOutput output = new MultipartRelatedOutput();
 
+        convertContentsFile();
+
         output.addPart(contentsFile, MediaType.valueOf("application/xml"), "foo.xml");
 
         for(String key : pageFiles.keySet())
         {
-            output.addPart(pageFiles.get(key), MediaType.valueOf("application/xml"), String.valueOf(pageFiles.get(key).hashCode()) + ".xml", "");
+            output.addPart(pageFiles.get(key), MediaType.valueOf("application/xml"), getStringHash(pageFiles.get(key)) + ".xml", "");
         }
 
         return output;
+    }
+
+    private void convertContentsFile()
+    {
+        NodeList pageNodes = contentsFile.getElementsByTagName("page");
+
+        for(int i = 0; i < pageNodes.getLength(); i++)
+        {
+            Node pageNode = pageNodes.item(i);
+
+            if(pageNode instanceof Element)
+            {
+                Element pageElement = (Element) pageNode;
+                String newFilename = getStringHash(pageFiles.get(pageElement.getAttribute("filename")));
+                pageElement.setAttribute("filename", newFilename + ".xml");
+            }
+        }
+    }
+
+    private String getStringHash(Document page)
+    {
+        return String.valueOf(page.hashCode());
     }
 }
