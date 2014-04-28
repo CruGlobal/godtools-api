f925f6559e3483ab82f7ec98a3381cef5dbac47a
ryancarlson <ryan.t.carlson@cru.org>
2014-04-28 14:20:24 -0400

    Fixed broken Meta endpoint

--- src/main/java/org/cru/godtools/api/meta/MetaResource.java
+++ src/main/java/org/cru/godtools/api/meta/MetaResource.java
@@ -15,7 +15,7 @@ import java.io.IOException;
  * Created by ryancarlson on 3/14/14.
  */
 
-@Path("/meta/{language}/{package : (/package)?}")
+@Path("/meta")
 public class MetaResource
 {
 
@@ -24,29 +24,52 @@ public class MetaResource
     @Inject
     AuthorizationService authService;
 
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public Response getAllMetaInfo(@QueryParam("interpreter") Integer minimumInterpreterVersionParam,
+										@HeaderParam("interpreter") Integer minimumInterpreterVersionHeader,
+										@QueryParam("authorization") String authCodeParam,
+										@HeaderParam("authorization") String authCodeHeader) throws ParserConfigurationException, SAXException, IOException
+	{
+		return getLanguageAndPackageMetaInfo(null, null, minimumInterpreterVersionParam, minimumInterpreterVersionHeader, authCodeParam, authCodeHeader);
+	}
+
     @GET
+	@Path("/{language}")
     @Produces(MediaType.APPLICATION_XML)
-    public Response getMetaInfo(@PathParam("language") String languageCode,
-                                @PathParam("package") String packageCode,
+    public Response getLanguageMetaInfo(@PathParam("language") String languageCode,
                                 @QueryParam("interpreter") Integer minimumInterpreterVersionParam,
                                 @HeaderParam("interpreter") Integer minimumInterpreterVersionHeader,
                                 @QueryParam("authorization") String authCodeParam,
                                 @HeaderParam("authorization") String authCodeHeader) throws ParserConfigurationException, SAXException, IOException
     {
-        authService.checkAuthorization(authCodeParam, authCodeHeader);
-        Integer interpreterVersion = getMinimumInterpreterVersion(minimumInterpreterVersionParam, minimumInterpreterVersionHeader);
-
-        if(interpreterVersion == null)
-        {
-            return Response.status(Response.Status.BAD_REQUEST)
-                    .entity(new ErrorResponse("parameter or header \"interpreter\" is required"))
-                    .build();
-        }
-
-        return Response.ok(metaService.getMetaResults(languageCode, packageCode, interpreterVersion)).build();
+       return getLanguageAndPackageMetaInfo(languageCode, null, minimumInterpreterVersionParam, minimumInterpreterVersionHeader, authCodeParam, authCodeHeader);
     }
 
-    private Integer getMinimumInterpreterVersion(Integer minimumInterpreterVersionParam, Integer minimumInterpreterVersionHeader)
+	@GET
+	@Path("/{language}/{package}")
+	@Produces(MediaType.APPLICATION_XML)
+	public Response getLanguageAndPackageMetaInfo(@PathParam("language") String languageCode,
+								@PathParam("package") String packageCode,
+								@QueryParam("interpreter") Integer minimumInterpreterVersionParam,
+								@HeaderParam("interpreter") Integer minimumInterpreterVersionHeader,
+								@QueryParam("authorization") String authCodeParam,
+								@HeaderParam("authorization") String authCodeHeader) throws ParserConfigurationException, SAXException, IOException
+	{
+		authService.checkAuthorization(authCodeParam, authCodeHeader);
+		Integer interpreterVersion = getMinimumInterpreterVersion(minimumInterpreterVersionParam, minimumInterpreterVersionHeader);
+
+		if(interpreterVersion == null)
+		{
+			return Response.status(Response.Status.BAD_REQUEST)
+					.entity(new ErrorResponse("parameter or header \"interpreter\" is required"))
+					.build();
+		}
+
+		return Response.ok(metaService.getMetaResults(languageCode, packageCode, interpreterVersion)).build();
+	}
+
+	private Integer getMinimumInterpreterVersion(Integer minimumInterpreterVersionParam, Integer minimumInterpreterVersionHeader)
     {
         return minimumInterpreterVersionParam == null ? minimumInterpreterVersionHeader : minimumInterpreterVersionParam;
     }
