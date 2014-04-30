78fd28456dc8cb6437ac452c36d18d53317437fa
ryancarlson <ryan.t.carlson@cru.org>
2014-04-30 14:19:34 -0400

    Remove image_resolutions, for now

--- src/main/resources/db/migration/V0_2__image_resoluions.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO image_resolutions(upper_bound, lower_bound, resolution) VALUES(13042, 320, 'High');
-INSERT INTO image_resolutions(upper_bound, lower_bound, resolution) VALUES(320, 240, 'Medium');
-INSERT INTO image_resolutions(upper_bound, lower_bound, resolution) VALUES(240, 160, 'Low');
