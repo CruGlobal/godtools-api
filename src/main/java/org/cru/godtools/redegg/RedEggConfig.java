fcd860ea0e7d37c58cdd4fb3d34249455ffd9cc6
mattfred <mrmattfred@gmail.com>
2014-07-03 15:14:50 -0400

    update to POM and new redeggconfig class (cherry picked from commit 26822b1)

--- src/main/java/org/cru/godtools/redegg/RedEggConfig.java
+++ src/main/java/org/cru/godtools/redegg/RedEggConfig.java
@@ -1,14 +1,14 @@
 package org.cru.godtools.redegg;
 
-import java.net.URISyntaxException;
+
 
 import com.google.common.collect.ImmutableList;
-import org.cru.crs.utils.CrsProperties;
 import org.cru.redegg.recording.api.NoOpParameterSanitizer;
 import org.cru.redegg.recording.api.ParameterSanitizer;
 import org.cru.redegg.reporting.errbit.ErrbitConfig;
 
 import javax.enterprise.inject.Produces;
+import java.net.URISyntaxException;
 import java.net.URI;
 
 /**
@@ -19,16 +19,14 @@ public class RedEggConfig
 
     @Produces ParameterSanitizer sanitizer = new NoOpParameterSanitizer();
 
-
-    public
-    @Produces ErrbitConfig createConfig() throws URISyntaxException
+    @Produces public ErrbitConfig createConfig() throws URISyntaxException
     {
         ErrbitConfig config = new ErrbitConfig();
         config.setEndpoint(new URI("https://errors.uscm.org"));
         config.setKey(properties.getProperty("errbitApiKey"));
         config.setEnvironmentName(properties.getProperty("errbitEnvironment"));
 
-        config.getApplicationBasePackage().addAll(ImmutableList.of("org.cru.goodTools"));
+        config.getApplicationBasePackages().addAll(ImmutableList.of("org.cru.goodTools"));
 
         return config;
     }
