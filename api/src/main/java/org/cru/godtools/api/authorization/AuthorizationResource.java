94af8131e4bd7819dd012686367860f6d0838d90
ryancarlson <ryan.t.carlson@cru.org>
2014-07-21 13:12:25 -0400

    Adding logging  - `MetaResource`  - `AuthorizationResource`

--- api/src/main/java/org/cru/godtools/api/authorization/AuthorizationResource.java
+++ api/src/main/java/org/cru/godtools/api/authorization/AuthorizationResource.java
@@ -1,10 +1,13 @@
 package org.cru.godtools.api.authorization;
 
 import org.ccci.util.time.Clock;
+import org.cru.godtools.domain.Simply;
 import org.cru.godtools.domain.authentication.AccessCodeRecord;
 import org.cru.godtools.domain.authentication.AuthTokenGenerator;
 import org.cru.godtools.domain.authentication.AuthorizationRecord;
 import org.cru.godtools.domain.authentication.AuthorizationService;
+import org.cru.godtools.domain.authentication.UnauthorizedException;
+import org.jboss.logging.Logger;
 import org.joda.time.DateTime;
 import org.xml.sax.SAXException;
 
@@ -33,42 +36,75 @@ public class AuthorizationResource
     @Inject
 	AuthorizationService authorizationService;
 
+	Logger log = Logger.getLogger(AuthorizationResource.class);
+
+	@GET
+	@Path("/status")
+	public Response requestAuthStatus(@HeaderParam("authorization") String authTokenParam,
+									  @QueryParam("authorization") String authTokenHeader)
+	{
+		// method will throw an UnauthorizedException (401) if:
+		// - authToken is not provided in one of two parameters,
+		// - authToken is invalid
+		// - authToken has been revoked
+		authorizationService.checkAuthorization(authTokenParam, authTokenHeader);
+
+		// if we get here, assume all is good and a 204 no content is fine.
+		return  Response.noContent().build();
+	}
+
     @POST
-    public Response requestTranslatorStatusWithoutCode(@HeaderParam("deviceId") String deviceIdHeader,
+    public Response getAuthorizationToken(@HeaderParam("deviceId") String deviceIdHeader,
                                             @QueryParam("deviceId") String deviceIdParam) throws ParserConfigurationException, SAXException, IOException
     {
-        String code = "";
-        return requestTranslatorStatus(code, deviceIdHeader, deviceIdParam);
-    }
+		log.info("Requesting basic authorization token");
+
+		AuthorizationRecord authorizationRecord = new AuthorizationRecord();
+
+		authorizationRecord.setId(UUID.randomUUID());
+		authorizationRecord.setDraftAccess(false);
+		authorizationRecord.setAuthToken(AuthTokenGenerator.generate());
+
+		String device = deviceIdHeader == null ? deviceIdParam : deviceIdHeader;
+		authorizationRecord.setDeviceId(device);
+
+		log.info("Saving basic authorization record:");
+		Simply.logObject(authorizationRecord, AuthorizationResource.class);
+
+		authorizationService.recordNewAuthorization(authorizationRecord);
+
+		return Response.noContent()
+				.header("authToken", authorizationRecord.getAuthToken())
+				.build();    }
 
 	@POST
-	@Path("/{code}")
-	public Response requestTranslatorStatus(@PathParam("code") String code,
+	@Path("/{translatorCode}")
+	public Response getAuthorizationToken(@PathParam("translatorCode") String translatorCode,
                                 @HeaderParam("deviceId") String deviceIdHeader,
                                 @QueryParam("deviceId") String deviceIdParam) throws ParserConfigurationException, SAXException,IOException
 	{
-		DateTime currentTime = clock.currentDateTime();
+		log.info("Requesting authorization token with translator access");
 
-        AccessCodeRecord accessCodeRecord = authorizationService.getAccessCode(code);
+		AccessCodeRecord accessCodeRecord = authorizationService.getAccessCode(translatorCode);
+		AuthorizationRecord authorizationRecord = new AuthorizationRecord();
+		String deviceId = deviceIdHeader == null ? deviceIdParam : deviceIdHeader;
 
-        AuthorizationRecord authorizationRecord = new AuthorizationRecord();
+		if(accessCodeRecord == null || !accessCodeRecord.isCurrentlyActive(clock.currentDateTime()))
+		{
+			log.info("Authorization with translator access code was invalid.");
+			log.info("Provided code: " + translatorCode);
+			log.info("Device ID: " + deviceId);
 
+			throw new UnauthorizedException();
+		}
+
+		authorizationRecord.setDraftAccess(true);
+		authorizationRecord.setId(UUID.randomUUID());
         authorizationRecord.setAuthToken(AuthTokenGenerator.generate());
+        authorizationRecord.setDeviceId(deviceId);
 
-        String device = deviceIdHeader == null ? deviceIdParam : deviceIdHeader;
-
-        authorizationRecord.setDeviceId(device);
-
-        authorizationRecord.setId(UUID.randomUUID());
-
-        if(accessCodeRecord != null && accessCodeRecord.isCurrentlyActive(currentTime))
-        {
-            authorizationRecord.setDraftAccess(true);
-        }
-        else
-        {
-            authorizationRecord.setDraftAccess(false);
-        }
+		log.info("Saving authorization record with translator access:");
+		Simply.logObject(authorizationRecord, AuthorizationResource.class);
 
         authorizationService.recordNewAuthorization(authorizationRecord);
 
@@ -76,14 +112,4 @@ public class AuthorizationResource
                 .header("authToken", authorizationRecord.getAuthToken())
                 .build();
 	}
-
-
-    @GET
-    @Path("/status")
-    public Response requestAuthStatus(@HeaderParam("authorization") String authTokenParam,
-                               @QueryParam("authorization") String authTokenHeader) throws ParserConfigurationException, SAXException,IOException
-    {
-        authorizationService.checkAuthorization(authTokenParam, authTokenHeader);
-        return  Response.noContent().build();
-    }
 }
