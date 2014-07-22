80ac4746c9641ce5925e583eb46c0f5a24e594cf
ryancarlson <ryan.t.carlson@cru.org>
2014-07-22 14:33:27 -0400

    Remove hardcoded database info, use properties

--- domain/src/test/java/org/cru/godtools/domain/UnittestDatabaseBuilder.java
+++ domain/src/test/java/org/cru/godtools/domain/UnittestDatabaseBuilder.java
@@ -2,12 +2,16 @@ package org.cru.godtools.domain;
 
 import com.googlecode.flyway.core.Flyway;
 import com.googlecode.flyway.core.api.MigrationVersion;
+import org.cru.godtools.domain.properties.GodToolsProperties;
+import org.cru.godtools.domain.properties.GodToolsPropertiesFactory;
 
 /**
  * Created by ryancarlson on 4/1/14.
  */
 public class UnittestDatabaseBuilder
 {
+	static GodToolsProperties properties = new GodToolsPropertiesFactory().get();
+
 	public static void main(String[] args)
 	{
 		new UnittestDatabaseBuilder().build();
@@ -16,7 +20,9 @@ public class UnittestDatabaseBuilder
 	public void build()
 	{
 		Flyway flyway = new Flyway();
-		flyway.setDataSource("jdbc:postgresql://localhost/godtoolstest", "godtoolsuser", "godtoolsuser");
+		flyway.setDataSource(properties.getProperty("unittestDatabaseUrl"),
+				properties.getProperty("unittestDatabaseUsername"),
+				properties.getProperty("unittestDatabasePassword"));
 		flyway.setInitVersion("0");
 		flyway.setTarget(MigrationVersion.fromVersion("0.3"));
 		flyway.clean();
