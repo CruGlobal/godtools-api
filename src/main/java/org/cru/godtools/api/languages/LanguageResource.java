ae5c03c95729c06db5c2d2223a6ecd2e536b739b
ryancarlson <ryan.t.carlson@cru.org>
2014-10-13 17:11:30 -0400

    did something here...

--- src/main/java/org/cru/godtools/api/languages/LanguageResource.java
+++ src/main/java/org/cru/godtools/api/languages/LanguageResource.java
@@ -1,7 +1,11 @@
 package org.cru.godtools.api.languages;
 
+import com.google.common.base.Optional;
 import org.cru.godtools.api.translations.GodToolsTranslationService;
 import org.cru.godtools.api.translations.NewTranslationCreation;
+import org.cru.godtools.domain.authentication.AccessCodeRecord;
+import org.cru.godtools.domain.authentication.AuthorizationRecord;
+import org.cru.godtools.domain.authentication.AuthorizationService;
 import org.cru.godtools.domain.languages.Language;
 import org.cru.godtools.domain.languages.LanguageCode;
 import org.cru.godtools.domain.languages.LanguageService;
@@ -25,12 +29,13 @@ public class LanguageResource
 {
 
 	@Inject
+	AuthorizationService authorizationService;
+	@Inject
 	LanguageService languageService;
 	@Inject
 	PackageService packageService;
 	@Inject
 	GodToolsTranslationService godToolsTranslationService;
-
 	@Inject
 	NewTranslationCreation translationCreation;
 
@@ -38,17 +43,21 @@ public class LanguageResource
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response addLanguage(Language language, @HeaderParam("Authorization") String authorization)
 	{
+		Optional<AuthorizationRecord> authorizationRecord = authorizationService.getAuthorizationRecord(authorization, "");
+
+		if(!authorizationRecord.isPresent() || !authorizationRecord.get().isAdmin()) return Response.status(401).build();
+
 		if(languageService.languageExists(language)) return Response.status(400).build();
 
 		language.setId(UUID.randomUUID());
 
 		languageService.insert(language);
 
-		for(org.cru.godtools.domain.packages.Package gtPackage : packageService.selectAllPackages())
-		{
-			godToolsTranslationService.setupNewTranslation(LanguageCode.fromLanguage(language),
-					gtPackage.getCode());
-		}
+//		for(org.cru.godtools.domain.packages.Package gtPackage : packageService.selectAllPackages())
+//		{
+//			godToolsTranslationService.setupNewTranslation(LanguageCode.fromLanguage(language),
+//					gtPackage.getCode());
+//		}
 		return Response.status(201).build();
 	}
 }
