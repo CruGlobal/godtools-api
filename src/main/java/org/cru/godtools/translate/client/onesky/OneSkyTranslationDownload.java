3ca08d8a05e22be07da638b2997de9c0f2991c86
ryancarlson <ryan.t.carlson@cru.org>
2014-12-09 10:57:58 -0500

    make it possible to disable onesky integration for non-prod environments

--- src/main/java/org/cru/godtools/translate/client/onesky/OneSkyTranslationDownload.java
+++ src/main/java/org/cru/godtools/translate/client/onesky/OneSkyTranslationDownload.java
@@ -1,5 +1,6 @@
 package org.cru.godtools.translate.client.onesky;
 
+import org.cru.godtools.domain.properties.GodToolsProperties;
 import org.cru.godtools.translate.client.TranslationResults;
 import org.cru.godtools.translate.client.TranslationDownload;
 import org.jboss.logging.Logger;
@@ -20,11 +21,20 @@ public class OneSkyTranslationDownload implements TranslationDownload
 		this.translationClient = translationClient;
 	}
 
+	@Inject
+	private GodToolsProperties properties;
+
 	private Logger log = Logger.getLogger(OneSkyTranslationUpload.class);
 
 	@Override
 	public TranslationResults doDownload(Integer oneSkyProjectId, String locale, String pageName)
 	{
+		if(!Boolean.parseBoolean(properties.getProperty("oneskyIntegrationEnabled", "true")))
+		{
+			log.info("Onesky integration disabled on this server.  Check configuration settings");
+			return new OneSkyTranslationResults();
+		}
+
 		log.info("Download translation file: " + pageName + " from OneSky for project ID: " + oneSkyProjectId + " and locale: " + locale);
 
 		return translationClient.export(oneSkyProjectId, locale, pageName);
@@ -33,6 +43,12 @@ public class OneSkyTranslationDownload implements TranslationDownload
 	@Override
 	public TranslationStatus checkTranslationStatus(Integer projectId, String locale, String pageName)
 	{
+		if(!Boolean.parseBoolean(properties.getProperty("oneskyIntegrationEnabled", "true")))
+		{
+			log.info("Onesky integration disabled on this server.  Check configuration settings");
+			return new OneSkyTranslationStatus();
+		}
+
 		return translationClient.getStatus(projectId, locale, pageName);
 	}
 }
