ddfdaaaaeded82f2139cf9ca98aa8ef72029e897
ryancarlson <ryan.t.carlson@cru.org>
2015-07-11 13:48:31 -0500

    Fix broken key generation code

--- src/main/java/org/cru/godtools/s3/AmazonS3GodToolsConfig.java
+++ src/main/java/org/cru/godtools/s3/AmazonS3GodToolsConfig.java
@@ -9,26 +9,27 @@ public class AmazonS3GodToolsConfig
 {
 	public static final String BUCKET_NAME = "cru-godtools";
 
-	private static final String META = "meta";
+	private static final String META = "meta/";
 	private static final String PACKAGES = "packages/";
 	private static final String LANGUAGES = "languages/";
+	private static final String META_FILE = "meta";
 	private static final String ALL = "all/";
 	private static final String CURRENT = "current/";
-	private static final String XML = ".XML";
-	private static final String ZIP = ".ZIP";
+	private static final String XML = ".xml";
+	private static final String ZIP = ".zip";
 
 	public static String getMetaKey(String languageCode, String packageCode)
 	{
 		if(!Strings.isNullOrEmpty(languageCode) && !Strings.isNullOrEmpty(packageCode))
 		{
-			return META + CURRENT + languageCode + "/" + packageCode + "/" + META + XML;
+			return META + CURRENT + languageCode + "/" + packageCode + "/" + META_FILE + XML;
 		}
 		if(!Strings.isNullOrEmpty(languageCode))
 		{
-			return META + CURRENT + languageCode + "/" + META + XML;
+			return META + CURRENT + languageCode + "/" + META_FILE + XML;
 		}
 
-		return META + CURRENT + META + XML;
+		return META + CURRENT + META_FILE + XML;
 	}
 
 	public static String getPackagesKey(String languageCode, String packageCode)
