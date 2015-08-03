588ca41ddd52706d7d8a3a13c9f083d9539b4d14
justinsturm <justin.sturm@cru.org>
2015-08-03 10:06:42 -0400

    Added test of DefaultLanguage

--- src/test/java/org/cru/godtools/domain/packages/PackageMockData.java
+++ src/test/java/org/cru/godtools/domain/packages/PackageMockData.java
@@ -1,5 +1,6 @@
 package org.cru.godtools.domain.packages;
 
+import org.cru.godtools.domain.languages.*;
 import org.cru.godtools.domain.services.*;
 import org.testng.Assert;
 
@@ -8,18 +9,39 @@ import org.testng.Assert;
  */
 public class PackageMockData
 {
-	public static void persistPackage(PackageService packageService)
+	public static Language persistLanguage(LanguageService languageService)
+	{
+
+		Language language = new Language();
+		language.setId(PackageServiceTest.TEST_LANGUAGE_ID);
+		language.setName("English");
+		languageService.insert(language);
+
+		return language;
+	}
+
+	public static void persistPackage(PackageService packageService, Language language)
 	{
 		Package gtPackage = new Package();
 		gtPackage.setId(PackageServiceTest.TEST_PACKAGE_ID);
 		gtPackage.setName("Test Package");
 		gtPackage.setCode("tp");
+		gtPackage.setDefaultLanguage(language);
 
 		packageService.insert(gtPackage);
 
 	}
 
 	public static void validatePackage(Package gtPackage)
+	{
+		Assert.assertNotNull(gtPackage);
+		Assert.assertEquals(gtPackage.getId(), PackageServiceTest.TEST_PACKAGE_ID);
+		Assert.assertEquals(gtPackage.getName(), "Test Package");
+		Assert.assertEquals(gtPackage.getCode(), "tp");
+		Assert.assertEquals(gtPackage.getDefaultLanguage().getId(), PackageServiceTest.TEST_LANGUAGE_ID);
+	}
+
+	public static void validateOrphanLanguagePackage(Package gtPackage)
 	{
 		Assert.assertNotNull(gtPackage);
 		Assert.assertEquals(gtPackage.getId(), PackageServiceTest.TEST_PACKAGE_ID);
--- src/test/java/org/cru/godtools/domain/packages/PackageServiceTest.java
+++ src/test/java/org/cru/godtools/domain/packages/PackageServiceTest.java
@@ -1,6 +1,7 @@
 package org.cru.godtools.domain.packages;
 
 import org.cru.godtools.domain.*;
+import org.cru.godtools.domain.languages.*;
 import org.cru.godtools.domain.services.*;
 import org.cru.godtools.domain.services.annotations.*;
 import org.cru.godtools.tests.*;
@@ -22,11 +23,13 @@ import java.util.UUID;
  */
 public class PackageServiceTest extends Arquillian
 {
+	public static final UUID TEST_LANGUAGE_ID = UUID.randomUUID();
 	public static final UUID TEST_PACKAGE_ID = UUID.randomUUID();
 
-	@Inject
-	@JPAStandard
+	@Inject @JPAStandard
 	PackageService packageService;
+	@Inject @JPAStandard
+	LanguageService languageService;
 
 	@Deployment
 	public static JavaArchive createDeployment()
@@ -50,14 +53,17 @@ public class PackageServiceTest extends Arquillian
 	public void setup()
 	{
 		packageService.setAutoCommit(false);
+		languageService.setAutoCommit(false);
 
-		PackageMockData.persistPackage(packageService);
+		Language language = PackageMockData.persistLanguage(languageService);
+		PackageMockData.persistPackage(packageService, language);
 	}
 
 	@AfterMethod
 	public void cleanup()
 	{
 		packageService.rollback();
+		languageService.rollback();
 	}
 
 	@Test
@@ -74,5 +80,15 @@ public class PackageServiceTest extends Arquillian
 		Package gtPackage = packageService.selectByCode("tp");
 
 		PackageMockData.validatePackage(gtPackage);
+	}
+
+	@Test
+	public void testOrphanedByLanguage()
+	{
+		languageService.rollback();
+
+		Package gtPackage = packageService.selectById(TEST_PACKAGE_ID);
+
+		PackageMockData.validateOrphanLanguagePackage(gtPackage);
 	}
 }
