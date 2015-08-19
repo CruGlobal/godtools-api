03dd54b0aca36b9ccf20322c67097e79f494dbec
ryancarlson <ryan.t.carlson@cru.org>
2015-08-19 17:31:03 -0400

    Add accepts xml to v1 package download
    - see javadoc for explanation

--- src/main/java/org/cru/godtools/api/packages/PackageResource.java
+++ src/main/java/org/cru/godtools/api/packages/PackageResource.java
@@ -44,10 +44,14 @@ public class PackageResource
 
 	/**
 	 * GET - get all packages for the language specified by @param languageCode.
+	 *
+	 * NOTE: "application/xml" has to be included as an accepted header b/c the android app currently passes that along.  the old version of this endpoint
+	 * could have returned just the XML "contents" or the whole zipped folder if compressed=true were passed.  Now it only returns the zipped content, but
+	 * Android clients who don't update their app will break b/c they currently & incorrectly pass an Accept: application/xml header
 	 */
     @GET
 	@Path("/{language}")
-    @Produces("application/zip")
+    @Produces({"application/zip", "application/xml"})
     public Response getAllPackagesForLanguage(@PathParam("language") String languageCode,
 											  @HeaderParam("interpreter") Integer minimumInterpreterVersionHeader,
 											  @QueryParam("compressed") String compressed,
