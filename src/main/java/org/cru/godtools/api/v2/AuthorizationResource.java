1c3ad0e7762a4ce20f79f43b8ed55297eea6ca0b
ryancarlson <ryan.t.carlson@cru.org>
2015-09-24 15:51:06 -0400

    Add getDraftToken endpoint to v2 API (iOS broken w/o it)

--- src/main/java/org/cru/godtools/api/v2/AuthorizationResource.java
+++ src/main/java/org/cru/godtools/api/v2/AuthorizationResource.java
@@ -7,11 +7,15 @@ import org.cru.godtools.domain.authentication.AuthorizationRecord;
 import org.cru.godtools.domain.authentication.AuthorizationService;
 import org.cru.godtools.domain.authentication.UnauthorizedException;
 import org.jboss.logging.Logger;
+import org.xml.sax.SAXException;
 
 import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.util.UUID;
 
 @Path("/v2/auth")
@@ -23,7 +27,39 @@ public class AuthorizationResource
 	@Inject
 	AuthorizationService authorizationService;
 
-	Logger log = Logger.getLogger(AuthorizationResource.class);
+	Logger log = Logger.getLogger(getClass());
+
+	@POST
+	@Path("/{translatorCode}")
+	public Response getDraftAccessToken(@PathParam("translatorCode") String translatorCode) throws ParserConfigurationException, SAXException,IOException
+	{
+		log.info("Requesting authorization token with translator access");
+
+		AccessCodeRecord accessCodeRecord = authorizationService.getAccessCode(translatorCode);
+
+		AuthorizationRecord authorizationRecord = createNewAuthorization();
+
+		if(accessCodeRecord == null || !accessCodeRecord.isCurrentlyActive(clock.currentDateTime()))
+		{
+			log.info("Authorization with translator access code was invalid.");
+			log.info("Provided code: " + translatorCode);
+
+			// If the incorrect translator code is given, a 401 is returned
+			throw new UnauthorizedException();
+		}
+
+		authorizationRecord.setDraftAccess(true);
+		authorizationRecord.setAdmin(false);
+
+		log.info(String.format("Saving authorization record with translator access w/ ID %s",
+				authorizationRecord.getId()));
+
+		authorizationService.recordNewAuthorization(authorizationRecord);
+
+		return Response.noContent()
+				.header("Authorization", authorizationRecord.getAuthToken())
+				.build();
+	}
 
 	@POST
 	@Path("/admin")
