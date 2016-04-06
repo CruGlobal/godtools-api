174014563082458212c2913eb394346383576085
Lael Watkins <lael.watkins@cru.org>
2016-04-06 06:38:25 -0400

    Added a test

--- src/test/java/org/cru/godtools/domain/PageStructureTest.java
+++ src/test/java/org/cru/godtools/domain/PageStructureTest.java
@@ -1,12 +1,11 @@
 package org.cru.godtools.domain;
 
-import org.ccci.util.xml.XmlDocumentSearchUtilities;
 import org.cru.godtools.domain.packages.PageStructure;
+import org.cru.godtools.tests.AbstractFullPackageServiceTest;
 import org.cru.godtools.utils.XmlDocumentFromFile;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -18,7 +17,6 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.StringReader;
 import java.io.StringWriter;
-import java.util.List;
 import java.util.UUID;
 
 /**
@@ -27,7 +25,7 @@ import java.util.UUID;
 public class PageStructureTest
 {
     @Test
-    public void testXmlParsing() throws Exception
+    public void testAddXmlToEnd() throws Exception
     {
         PageStructure pageStructure = new PageStructure();
         pageStructure.setId(UUID.randomUUID());
@@ -57,6 +55,9 @@ public class PageStructureTest
         builder = factory.newDocumentBuilder();
         Document additionsXmlDocument = builder.parse(new InputSource(new StringReader(xmlAdditions)));
 
+        pageStructure.setId(AbstractFullPackageServiceTest.PAGE_STRUCTURE_ID);
+        pageStructure.setTranslationId(AbstractFullPackageServiceTest.TRANSLATION_ID);
+        pageStructure.setDescription("test_file_1.xml");
         pageStructure.setXmlContent(XmlDocumentFromFile.get("/test_file_1.xml"));
         pageStructure.addXmlContent(additionsXmlDocument);
 
@@ -79,5 +80,83 @@ public class PageStructureTest
             theDocumentsAreEqual = true;
 
         Assert.assertTrue(theDocumentsAreEqual);
+    }
+
+    @Test
+    public void testAddXmlInsert() throws Exception
+    {
+        PageStructure pageStructure = new PageStructure();
+        pageStructure.setId(UUID.randomUUID());
+
+        String xmlAdditions = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+                "<!-- NOTE: this structure does not represent a valid GodTools XML file, just here to provide test cases for various utilities -->\n" +
+                "<languages>\n" +
+                "    <language code=\"en\">\n" +
+                "        <package code=\"kgp\" >\n" +
+                "            <name>Knowing God Personally</name>\n" +
+                "        </package>\n" +
+                "        <package code=\"satisfied\">\n" +
+                "            <name>Satisfied?</name>\n" +
+                "        </package>\n" +
+                "    </language>\n" +
+                "</languages>";
+
+        String xmlAdditions2 = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+                "<!-- NOTE: this structure does not represent a valid GodTools XML file, just here to provide test cases for various utilities -->\n" +
+                "<languages>\n" +
+                "    <language code=\"en\">\n" +
+                "        <package code=\"kgp\" >\n" +
+                "            <name>Knowing God Personally</name>\n" +
+                "        </package>\n" +
+                "        <package code=\"satisfied\">\n" +
+                "            <name>Satisfied?</name>\n" +
+                "        </package>\n" +
+                "        <package code=\"fourlaws\">\n" +
+                "            <name>Four Laws</name>\n" +
+                "        </package>\n" +
+                "    </language>\n" +
+                "</languages>";
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilderFactory factory2 = DocumentBuilderFactory.newInstance();
+
+        DocumentBuilder builder;
+        DocumentBuilder builder2;
+
+        builder = factory.newDocumentBuilder();
+        builder2 = factory2.newDocumentBuilder();
+
+        Document originalXmlDocument = builder.parse(new InputSource(new StringReader(xmlAdditions)));
+        Document additionsXmlDocument = builder2.parse(new InputSource(new StringReader(xmlAdditions2)));
+
+        originalXmlDocument.normalize();
+        additionsXmlDocument.normalize();
+
+        pageStructure.setXmlContent(originalXmlDocument);
+        pageStructure.addXmlContent(additionsXmlDocument);
+
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+
+        StreamResult result = new StreamResult(new StringWriter());
+        StreamResult result1 = new StreamResult(new StringWriter());
+
+        pageStructure.getXmlContent().normalizeDocument();
+        DOMSource source = new DOMSource(pageStructure.getXmlContent());
+        DOMSource source1 = new DOMSource(additionsXmlDocument);
+
+        transformer.transform(source, result);
+        transformer.transform(source1, result1);
+
+        String xmlOutput = result.getWriter().toString();
+        String xmlOutput2 = result1.getWriter().toString();
+        System.out.println(xmlOutput.replace("\\s", "") );
+        System.out.println(xmlOutput2.replace("\\s", ""));
+
+        boolean theDocumentsAreEqual = false;
+
+        if(xmlOutput.replace("\\s", "").equals(xmlOutput2.replace("\\s", "")))
+            theDocumentsAreEqual = true;
+
+        Assert.assertEquals(xmlOutput.replace("\\s", ""),xmlOutput2.replace("\\s", ""));
     }
 }
