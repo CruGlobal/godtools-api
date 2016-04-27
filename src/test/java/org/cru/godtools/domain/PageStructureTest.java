10590d7c6bf1a981cb0c21ce8a1be6521d6e4d0f
Lael Watkins <lael.watkins@cru.org>
2016-04-27 11:48:14 -0400

    Another fix for logic to handle node inserts when the new node is the first child. This time using the importNode method Updated the test too.

--- src/test/java/org/cru/godtools/domain/PageStructureTest.java
+++ src/test/java/org/cru/godtools/domain/PageStructureTest.java
@@ -145,20 +145,16 @@ public class PageStructureTest
         pageStructure.setId(UUID.randomUUID());
 
         String xmlAdditions = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"+
-                "<page color=\"#00759A\" watermark=\"Home_Watermark.png\">\n"+
-                "<text color=\"#FFFFFF\" gtapi-trx-id=\"da76705f-187d-4083-8677-593a1b7c58fd\" size=\"136\" textalign=\"center\" translate=\"true\" w=\"300\" xoffset=\"40\" y=\"50\" />\n"+
-                "<text color=\"#FFFFFF\" gtapi-trx-id=\"be73de97-89fe-4490-a686-220546e2592c\" modifier=\"bold\" size=\"156\" textalign=\"center\" translate=\"true\" w=\"300\" yoffset=\"-50\" />\n"+
-                "<text color=\"#FFFFFF\" gtapi-trx-id=\"404edd90-1aff-4bb9-83a2-f0d15c46a6e8\" modifier=\"bold\" size=\"156\" textalign=\"center\" translate=\"true\" w=\"300\" xoffset=\"-34\" yoffset=\"-50\" />\n"+
-                "<text alpha=\"0.8\" color=\"#ffffff\" gtapi-trx-id=\"d4b40701-5929-498b-b96e-cc6796d76771\" modifier=\"italics\" size=\"112\" textalign=\"center\" translate=\"true\" w=\"300\" yoffset=\"140\" />\n"+
+                "<page backgroundimage=\"wave.png\" color=\"#DBF0FC\">\n" +
+                "<text color=\"#007486\" gtapi-trx-id=\"34cc87cd-64cd-49a9-a12b-b6b06e4acdbf\" modifier=\"bold\" size=\"100\" textalign=\"center\" translate=\"true\" w=\"300\" y=\"200\"> </text>\n" +
+                "<text color=\"#007486\" gtapi-trx-id=\"99a12a3b-0b53-4059-92eb-53496438c3de\" modifier=\"italics\" size=\"240\" textalign=\"center\" translate=\"true\" w=\"300\" y=\"245\"> </text>\n" +
                 "</page>";
 
         String xmlAdditions2 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"+
-                "<page color=\"#00759A\" watermark=\"Home_Watermark.png\">\n"+
-                "<text color=\"#FFFFFF\" gtapi-trx-id=\"ua10000f-187d-4083-8677-593a1t7c58fd\" size=\"136\" textalign=\"center\" translate=\"true\" w=\"300\" xoffset=\"300\" y=\"50\" />\n"+
-                "<text color=\"#FFFFFF\" gtapi-trx-id=\"da76705f-187d-4083-8677-593a1b7c58fd\" size=\"136\" textalign=\"center\" translate=\"true\" w=\"300\" xoffset=\"40\" y=\"50\" />\n"+
-                "<text color=\"#FFFFFF\" gtapi-trx-id=\"be73de97-89fe-4490-a686-220546e2592c\" modifier=\"bold\" size=\"156\" textalign=\"center\" translate=\"true\" w=\"300\" yoffset=\"-50\" />\n"+
-                "<text color=\"#FFFFFF\" gtapi-trx-id=\"404edd90-1aff-4bb9-83a2-f0d15c46a6e8\" modifier=\"bold\" size=\"156\" textalign=\"center\" translate=\"true\" w=\"300\" xoffset=\"-34\" yoffset=\"-50\" />\n"+
-                "<text alpha=\"0.8\" color=\"#ffffff\" gtapi-trx-id=\"d4b40701-5929-498b-b96e-cc6796d76771\" modifier=\"italics\" size=\"112\" textalign=\"center\" translate=\"true\" w=\"300\" yoffset=\"140\" />\n"+
+                "<page backgroundimage=\"wave.png\" color=\"#DBF0FC\">\n" +
+                "<text color=\"#007486\" gtapi-trx-id=\"364fb3eb-7a2d-40a3-bd19-32412af6df70\" modifier=\"italics\" size=\"340\" textalign=\"center\" translate=\"true\" w=\"500\" y=\"945\"> </text>\n" +
+                "<text color=\"#007486\" gtapi-trx-id=\"34cc87cd-64cd-49a9-a12b-b6b06e4acdbf\" modifier=\"bold\" size=\"100\" textalign=\"center\" translate=\"true\" w=\"300\" y=\"200\"> </text>\n" +
+                "<text color=\"#007486\" gtapi-trx-id=\"99a12a3b-0b53-4059-92eb-53496438c3de\" modifier=\"italics\" size=\"240\" textalign=\"center\" translate=\"true\" w=\"300\" y=\"245\"> </text>\n" +
                 "</page>";
 
         Document originalXmlDocument = createDocumentFromString(xmlAdditions);
